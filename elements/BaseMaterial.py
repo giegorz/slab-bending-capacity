840c33a27e51aa3e9384f46644ef56876160de64
GrzegorzCzerpak <Grzegorz.Czerpak@jacobs.com>
2026-05-27 11:56:21 +0200

    improved material class

--- elements/BaseMaterial.py
+++ elements/BaseMaterial.py
@@ -1,30 +1,80 @@
-from abc import abstractmethod
-from dataclasses import dataclass
+from abc import abstractmethod, ABC
+from dataclasses import dataclass, field
+from typing import Callable
+
 import numpy as np
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
 from elements.units import *
 
 
 @dataclass
-class BaseMaterial:
+class BaseMaterial(ABC):
     pass
 
     @abstractmethod
     def strain_stress(self, eps: float | np.ndarray) -> float | np.ndarray:
         pass
 
+    def plot_strain_stress(
+            self,
+            *,
+            eps_end: float = 0.0035,
+            number_of_points: int = 500
+    ) -> Figure:
+
+        fig, ax = plt.subplots()
+
+        if isinstance(self, SteelCS454):
+            eps_end = 0.01
+
+        x = np.linspace(0, eps_end, number_of_points)
+        y = self.strain_stress(x)
+        plt.plot(x, y)
+
+        return fig
+
 @dataclass
 class SteelCS454(BaseMaterial):
-    fyk: Quantity
+    fyk: float
     E: float = 200_000
     gamma_m: float = 1.5
 
     def strain_stress(self, eps: float | np.ndarray) -> float | np.ndarray:
-        return eps * self.E
+        eps = np.asarray(eps)
+
+        s1 = 0.8 * self.fyk / self.gamma_m
+        s2 = self.fyk / self.gamma_m
+        e1 = s1 / self.E
+        e2 = s2 / self.E + 0.002
+
+        part_1 = eps * self.E
+        part_2 = (s2 - s1) / (e2 - e1) * (eps - e1) + \
+                 0.8 * self.fyk / self.gamma_m
+        part_3 = s2
+
+        mask_1 = eps < e1
+        mask_2 = (e1 <= eps) & (eps < e2)
+        mask_3 = eps >= e2
+
+        result = np.zeros_like(eps)
+        result[mask_1] = part_1[mask_1]
+        result[mask_2] = part_2[mask_2]
+        result[mask_3] = part_3
+
+        return result
 
     @property
     def fyd(self) -> float:
-        return self.fyk / self.gamma_m
+        try:
+            return self.fyk / self.gamma_m
+        except ZeroDivisionError:
+            raise ZeroDivisionError(f"gamma cannot be zero!")
+
+    @fyd.setter
+    def fyd(self, value):
+        self._fyd = value
 
 
 @dataclass
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from matplotlib.figure import Figure
+
 from elements.BaseElements import *
-from elements.BaseMaterial import ConcreteCS454
+from elements.BaseMaterial import ConcreteCS454, SteelCS454
 from elements.units import Quantity, ureg, from_Quantity
 import numpy as np
 import logging
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger(__name__)
+
+def plot(x, y)  -> Figure:
+    fig, ax = plt.subplots()
+
+    plt.plot(x, y)
+    return fig
 
 
 def main ():
@@ -13,6 +22,8 @@ def main ():
     layer = ReinforcementLayer([bar, bar2], [10, 5])
     print(bar.d, bar2.d)
     print(layer.area)
+    steel = SteelCS454(435)
+
 
     concrete = ConcreteCS454(fcu= from_Quantity(35*ureg.MPa))
     print(concrete.E)
@@ -24,6 +35,13 @@ def main ():
 
     slab = Slab(from_Quantity(720*ureg.mm), from_Quantity(1000*ureg.mm))
     print(slab.area)
+
+    xz = np.linspace(0, 0.010, 500)
+    z = steel.strain_stress(xz)
+
+
+    steel.plot_strain_stress().show()
+    concrete.plot_strain_stress().show()
 
 
 if __name__ == "__main__":
